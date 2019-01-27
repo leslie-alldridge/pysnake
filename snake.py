10bedcd07077e752479124e76cb86e856a79aa22
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-01-27 21:24:54 +1300

    grid displays
    grid displays

--- snake.py
+++ snake.py
@@ -33,10 +33,23 @@ class snake(object):
         pass
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
+
 
 def redrawWindow(surface):
-    pass
+    global rows, width
+    surface.fill((0,0,0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
 
 def randomSnack(rows, items):
     pass
@@ -45,16 +58,20 @@ def message_box(subject, content):
     pass
 
 def main():
+    global width, rows
     width = 500
-    height = 500
     rows = 20
-    win = pygame.display.set_mode((width, height))
+    win = pygame.display.set_mode((width, width))
     s = snake((255,0,0), (10,10))
     flag = True
+
     clock = pygame.time.Clock()
+
     while flag: 
+        pygame.event.get()
         pygame.time.delay(50)
         clock.tick(10)
+        redrawWindow(win)
     pass
 
 main()
